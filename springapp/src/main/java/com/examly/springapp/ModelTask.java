84d160f948d0975a76e29a66780dc73b98c4902d
Iamneo <techadmin@iamneo.ai>
2023-08-01 09:36:07 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/ModelTask.java
+++ springapp/src/main/java/com/examly/springapp/ModelTask.java
@@ -1,59 +1,44 @@
 package com.examly.springapp;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Task {
+public class ModelTask {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long taskId;
-    private String taskHolderName;
-    private String taskDate;
-    private String taskName;
-    private String taskStatus;
-
-    // Getters and Setters
-
-    public Long getTaskId() {
+    int taskId;
+    String taskHolderName;
+    String taskDate;
+    String taskName;
+    String taskStatus; 
+    public int getTaskId() {
         return taskId;
     }
-
-    public void setTaskId(Long taskId) {
+    public void setTaskId(int taskId) {
         this.taskId = taskId;
     }
-
     public String getTaskHolderName() {
         return taskHolderName;
     }
-
     public void setTaskHolderName(String taskHolderName) {
         this.taskHolderName = taskHolderName;
     }
-
     public String getTaskDate() {
         return taskDate;
     }
-
     public void setTaskDate(String taskDate) {
         this.taskDate = taskDate;
     }
-
     public String getTaskName() {
         return taskName;
     }
-
     public void setTaskName(String taskName) {
         this.taskName = taskName;
     }
-
     public String getTaskStatus() {
         return taskStatus;
     }
-
     public void setTaskStatus(String taskStatus) {
         this.taskStatus = taskStatus;
-    }
+    } 
 }
